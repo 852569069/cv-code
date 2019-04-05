a3135b4b8f6b61809bca25dddff92bf33a7e2149
852569069 <carzysnailq@users.noreply.github.com>
2019-04-05 15:47:10 +0800

    初步完成，能运行代码

--- cv/写诗代码.py
+++ cv/写诗代码.py
@@ -1,9 +1,9 @@
-
 import tensorflow as tf
 import os
 import collections
 import numpy as np
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow.contrib as con
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 class data_deal(object):
     def __init__(self):
@@ -53,11 +53,16 @@ class data_deal(object):
             elif len(i)>40 and len(i)<80:
                 i=np.pad(np.array(i),[0,80-len(i)],'constant',constant_values=(7559))
                 all_poem.append(i)
-        result_poem=[]
-        for l in all_poem:
-            change=l[1:-1]
-            # l[0:-2]=change
-            result_poem.append(change)
+
+        result_poem=np.copy(all_poem)
+        """
+        此处注意,需要复制一份出来，
+        否则会改变原来地址中的值，略抽象，慢慢体会。
+        
+        """
+        for l in result_poem:
+            change=l[1:]
+            l[0:-1]=change
         return np.array(all_poem),result_poem
 
 
@@ -68,43 +73,72 @@ class auto_poem(object):
         self.word_num=7600
         self.poem_num=poem_num
         self.num_layer=2
-
-
         pass
     def lstm(self,input):
         input=tf.placeholder(tf.int32,[self.batch_size,80])
-        embedding_vari=tf.random_normal([self.word_num,128])
-        embedding_code=tf.nn.embedding_lookup(embedding_vari,input)
+        output=tf.placeholder(tf.int32,[self.batch_size,80])
+        with tf.variable_scope('embedding',initializer=tf.random_uniform_initializer(-1,1)):
+            embedding_vari=tf.get_variable('embedding',[self.word_num,128],tf.float32)
+            embedding_code=tf.nn.embedding_lookup(embedding_vari,input)
+        with tf.variable_scope('lstm',reuse=tf.AUTO_REUSE):
+            basic_rnn=tf.nn.rnn_cell.BasicLSTMCell(num_units=128)
+            """
+            #一个很重要的点，这个num_unit可以理解为LSTM节点内的神经元的个数。
+            # 可以参考：https://www.zhihu.com/question/64470274。所以输入的数据是【batch_size,80,128】的数据。
+            # 其中的80是有80个time_step.
+            # 然后128也就是我们embedding编码之后的维度，就必须和神经元的个数对应。
+            """
+            muti_lstm=tf.nn.rnn_cell.MultiRNNCell([basic_rnn for i in range(1)])
+            out,_=tf.nn.dynamic_rnn(muti_lstm,embedding_code,
+                                    initial_state=muti_lstm.zero_state
+                                    (self.batch_size,tf.float32))
 
-        basic_rnn=tf.nn.rnn_cell.BasicLSTMCell(num_units=128)
+        #此处输出的结构时【100，80，128】，将其reshape成【-1，128】的结构的。也就是【8000，128】的结构。
+        # 再连接一个全连接层。对应到分类上。
+
+        out=tf.reshape(out,[-1,self.embedding_size])
+        out=tf.layers.dense(out,self.word_num)
+        #此时，输出的结构时【8000，7600】.前者的8000时batch_size*time_step.
+        # 也就是说，将batch_size个诗句中的所有的词都合并在一起。而且对应的结构是，【【诗句1】【诗句2】【诗句3】。。。。】。
+        # 其中诗句里面是【字1，字2，字3.。。。】。
+        # 所以说里面的每一个元素都对应一个长度为【7600】的词库里面的一个词。
+        # 可以参考：https://blog.csdn.net/xyz1584172808/article/details/83056179
+        #output的结构是【100，80】，也就是说对应了正确的那个字。我们也将其做一个reshape。
+        out_poem=tf.reshape(output,[-1])
         """
-        #一个很重要的点，这个num_unit可以理解为LSTM节点内的神经元的个数。
-        # 可以参考：https://www.zhihu.com/question/64470274。所以输入的数据是【batch_size,80,128】的数据。
-        # 其中的80是有80个time_step.
-        # 然后128也就是我们embedding编码之后的维度，就必须和神经元的个数对应。
+        此处必须传入list结构的tensor。
+        List of 2D Tensors of shape [batch_size x num_decoder_symbols]
         """
-        muti_lstm=tf.nn.rnn_cell.MultiRNNCell([basic_rnn for i in range(self.num_layer)])
-        _,out=tf.nn.dynamic_rnn(muti_lstm,embedding_code,initial_state=muti_lstm.zero_state(self.batch_size,tf.float32))
+        loss=con.legacy_seq2seq.sequence_loss_by_example(logits=[out],
+                                                         targets=[out_poem],
+                                                         weights=[tf.ones_like([8000,],
+                                                        dtype=tf.float32)])
+        self.loss=tf.reduce_mean(loss)
+
+        self.tvars = tf.trainable_variables()
+        self.grads,_=tf.clip_by_global_norm(tf.gradients(self.loss,self.tvars),5)
 
 
-        return input,out
-
-
-
+        return input,output,self.loss,self.tvars
+    def train_op(self):
+        train=tf.train.AdamOptimizer(0.0001).apply_gradients(zip(self.grads,self.tvars))
+        return train
 data_deal=data_deal()
 result,final_poem=data_deal.poem_len_control()
-print(result[24])
-print(final_poem[24])
-# poem_num=len(result)
-# auto_poem=auto_poem(poem_num)
-# input_place,embedding=auto_poem.lstm(result)
-# sess=tf.Session()
-# sess.run(tf.global_variables_initializer())
-# all=sess.run(embedding,{input_place:result[0:100]})
-# print(all)
 
+poem_num=len(result)
+auto_poem=auto_poem(poem_num)
+input_place,output_place,loss,tvar=auto_poem.lstm(result)
+print(tvar)
 
+for i in tvar:
+    print(i)
+train_op=auto_poem.train_op()
 
-# embedding_vari=tf.random_normal()
-#
-# tf.nn.embedding_lookup
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+for i in range(100):
+    fetch=[loss,train_op]
+    all=sess.run(fetch,{input_place:result[0:100],output_place:final_poem[0:100]})
+    print(all[0])
+
