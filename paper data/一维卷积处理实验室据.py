765bb76230de9e4fb95e10a303095fd04a042bc8
852569069 <30398478+852569069@users.noreply.github.com>
2019-04-18 12:12:07 +0800

    data

--- paper data/一维卷积处理实验室据.py
+++ paper data/一维卷积处理实验室据.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import os
 import numpy as np
 import pandas as pd
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 filename1='data11.csv'
@@ -19,8 +19,6 @@ data3_=np.hstack([data3,np.zeros([279,1],dtype=np.int)+2])
 data4=pd.read_csv(filename4,header=None)
 data4_=np.hstack([data4,np.zeros([279,1],dtype=np.int)+3])
 data_all=np.vstack([data1_,data2_,data3_,data4_])
-
-
 
 class Data(object):
     """
@@ -41,8 +39,15 @@ class Data(object):
             x_data.append(list(i[0:50]))
             y_data.append(i[-1])
         return list(x_data), y_data
+    def creat_text_data(self,input):
+        np.random.shuffle(input)
+        point=int(len(input)*0.8)
+        train_data=input[0:point]
+        test_data=input[-point:]
+        return train_data,test_data
+
     def batch(self,input):
-        if self.indicator>len(input):
+        if self.indicator>len(input)-10:
             np.random.shuffle(input)
             x_data, y_data = self.__data_spe(input)
             self.indicator=0
@@ -58,28 +63,46 @@ def con1d():
     input_pla=tf.placeholder(tf.float32,[10,50])
     labels=tf.placeholder(tf.int32,[10,])
     input=tf.reshape(input_pla,[10,50,1])
-    con1d=tf.layers.conv1d(input,32,1)
+    input=tf.layers.batch_normalization(input)#批归一化就是将所有的数值集中在均值为0，方差为1的区间内。
+    con1d=tf.layers.conv1d(input,32,4)
+    # con1d=tf.layers.batch_normalization(con1d)
     con1d=tf.layers.max_pooling1d(con1d,2,2)
-    con1d=tf.layers.conv1d(con1d,64,1)
+    con1d=tf.layers.conv1d(con1d,64,3)
     con1d=tf.layers.max_pooling1d(con1d,2,2)
+    con1d = tf.layers.conv1d(con1d, 128, 2)
+    con1d = tf.layers.max_pooling1d(con1d, 2, 2)
+
+
     con1d=tf.layers.flatten(con1d)
-    dese=tf.layers.dense(con1d,500,activation=tf.nn.relu)
+    dese=tf.layers.dense(con1d,1024,activation=tf.nn.sigmoid)
     # dese=tf.layers.dropout(dese)
-    dese=tf.layers.dense(dese,100,activation=tf.nn.sigmoid)
+    dese=tf.layers.dense(dese,512,activation=tf.nn.sigmoid)
+    dese=tf.layers.dense(dese,256,activation=tf.nn.sigmoid)
     dese=tf.layers.dense(dese,4,activation=tf.nn.softmax)
     loss=tf.losses.sparse_softmax_cross_entropy(labels,dese)
+    tf.summary.scalar('loss',loss)
     acc=tf.metrics.accuracy(labels,tf.argmax(dese,axis=1))
-    train_op=tf.train.AdamOptimizer(0.0001).minimize(loss)
+    tf.summary.scalar('acc',acc[-1])
+    train_op=tf.train.AdamOptimizer(0.0006).minimize(loss)
     sess=tf.Session()
+    write=tf.summary.FileWriter('test',sess.graph)
+    meged=tf.summary.merge_all()
     saver=tf.train.Saver()
     sess.run([tf.global_variables_initializer(),tf.local_variables_initializer()])
-    for i in range(10000):
-        d = Data()
-        x, y = d.batch(data_all)
+    ckp=tf.train.latest_checkpoint('./ckp_TEST/')
+    if ckp is not None:
+        saver.restore(sess,ckp)
+    d = Data()
+    train_data, test_data = d.creat_text_data(data_all)
+    for i in range(1000000):
+        x, y = d.batch(train_data)
         result=sess.run([train_op,acc,loss],{input_pla:x,labels:y})
         if i%300==0:
+            all=sess.run(meged,{input_pla:x,labels:y})
+            write.add_summary(all,i)
             saver.save(sess,'./ckp_TEST/ckp%d'%i)
         print(result[-1])
         print(result[-2][-1])
+
 if __name__ == '__main__':
     con = con1d()
